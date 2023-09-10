93952a9cab02df8b7944a2d41d98bd2598c42d2a
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-09-10 16:04:19 +0300

    1:35:00

---
+++ app/Filament/Resources/PetResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\PetType;
+use App\Filament\Resources\PetResource\Pages;
+use App\Filament\Resources\PetResource\RelationManagers;
+use App\Models\Pet;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PetResource extends Resource
+{
+    protected static ?string $model = Pet::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make([
+                    Forms\Components\FileUpload::make('avatar')
+                        ->image()
+                        ->imageEditor(),
+                    Forms\Components\TextInput::make('name')
+                        ->required(),
+                    Forms\Components\DatePicker::make('date_of_birth')
+                        ->native(false)
+                        ->required()
+                        ->displayFormat('Y-m-d'),
+                    Forms\Components\Select::make('type')
+                        ->native(false)
+                        ->options(PetType::class),
+                ])->maxWidth('lg'),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPets::route('/'),
+            'create' => Pages\CreatePet::route('/create'),
+            'edit' => Pages\EditPet::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Pet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pet extends Model
+{
+    use HasFactory;
+
+    protected static $unguarded = true;
+
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
+}
