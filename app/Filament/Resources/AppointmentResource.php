da5892fb59ed3d56e6ccaa0c7bda330bc2d3eac2
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-01-16 17:54:23 +0300

    01 - 2:38:10

---
+++ app/Filament/Resources/AppointmentResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AppointmentResource\Pages;
+use App\Filament\Resources\AppointmentResource\RelationManagers;
+use App\Models\Appointment;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AppointmentResource extends Resource
+{
+    protected static ?string $model = Appointment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make([
+                    Forms\Components\DatePicker::make('date')
+                        ->native(false)
+                        ->required(),
+                    Forms\Components\TimePicker::make('start')
+                        ->required()
+                        ->seconds(false)
+                        ->minutesStep(10),
+                    Forms\Components\TimePicker::make('end')
+                        ->required()
+                        ->seconds(false),
+                    Forms\Components\Select::make('pet_id')
+                        ->required()
+                        ->relationship('pet', 'name')
+                        ->searchable()
+                        ->preload(),
+                    Forms\Components\TextInput::make('description')
+                        ->required(),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('pet.name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('date')
+                    ->date('d M Y')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('start')
+                    ->time()
+                    ->label('From')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('end')
+                    ->time()
+                    ->label('From')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAppointments::route('/'),
+            'create' => Pages\CreateAppointment::route('/create'),
+            'edit' => Pages\EditAppointment::route('/{record}/edit'),
+        ];
+    }
+}
