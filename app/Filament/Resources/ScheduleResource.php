8fa527ba4a22f8c56990fe56c2a61a67bd9881ef
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-01-18 14:02:08 +0300

    04 - 1:14:25

---
+++ app/Filament/Resources/ScheduleResource.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ScheduleResource\Pages;
+use App\Filament\Resources\ScheduleResource\RelationManagers;
+use App\Models\Role;
+use App\Models\Schedule;
+use App\Models\Slot;
+use Carbon\Carbon;
+use Faker\Provider\Text;
+use Filament\Facades\Filament;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ScheduleResource extends Resource
+{
+    protected static ?string $model = Schedule::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        $doctorRole = Role::whereName('doctor')->first();
+        return $form
+            ->schema([
+                Forms\Components\Section::make([
+                    Forms\Components\DatePicker::make('date')
+                        ->native(false)
+                        ->required(),
+                    Forms\Components\Select::make('owner_id')
+                        ->native(false)
+                        //->relationship('owner', 'name')
+                        ->options(Filament::getTenant()
+                            ->users()
+                            ->whereBelongsTo($doctorRole)
+                            ->get()
+                            ->pluck('name', 'id')
+                        )
+                        ->required(),
+                    Forms\Components\Repeater::make('slots')
+                        ->relationship()
+                        ->schema([
+                            Forms\Components\TimePicker::make('start')
+                                ->native(false)
+                                ->seconds(false)
+                                ->required(),
+                            Forms\Components\TimePicker::make('end')
+                                ->native(false)
+                                ->seconds(false)
+                                ->required(),
+                        ]),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->groups([
+                Tables\Grouping\Group::make('date')
+                    ->collapsible(),
+            ])
+            ->defaultGroup('date')
+            ->groupingSettingsInDropdownOnDesktop()
+            ->columns([
+                Tables\Columns\TextColumn::make('date')
+                    ->date()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('owner.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('slots.start')
+                    ->badge()
+                    ->formatStateUsing(fn(Carbon $state) => Carbon::parse($state)->format('H::i')),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSchedules::route('/'),
+            'create' => Pages\CreateSchedule::route('/create'),
+            'edit' => Pages\EditSchedule::route('/{record}/edit'),
+        ];
+    }
+}
